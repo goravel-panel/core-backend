450372bbdec7f5d60c86906a60a34f8fed44188e
kkumar-gcc <krishkumar9352@gmail.com>
2025-03-06 13:13:34 +0530

    update readme

--- serve.sh
+++ serve.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-# 获取 .env 文件里配置的端口号
+# Get the port number configured in the .env file
 source .env
 port=$APP_PORT
 
-# 监听端口号是否被占用
+# Check if the port is being used
 if netstat -an | grep "$port" | grep -i listen > /dev/null ; then
     lsof -i:"$port" | grep main
-    echo -e "\033[1;31m$port 端口被占用，请输入被占用的进程PID: \033[0m"
-    read -r  pid
+    echo -e "\033[1;31mPort $port is in use. Please enter the PID of the process occupying the port: \033[0m"
+    read -r pid
     kill -15 "$pid"
     if [ $? -eq 0 ]; then
-        echo -e "\033[1;32m进程($pid)终止成功，正重启服务··· \033[0m"
+        echo -e "\033[1;32mProcess ($pid) terminated successfully. Restarting the service... \033[0m"
         sleep 1
         air
     else
-        echo -e "\033[1;31m进程($pid)终止失败，请检查输入的进程ID是否正确 \033[0m"
+        echo -e "\033[1;31mFailed to terminate process ($pid). Please check if the process ID entered is correct. \033[0m"
     fi
 else
    air
